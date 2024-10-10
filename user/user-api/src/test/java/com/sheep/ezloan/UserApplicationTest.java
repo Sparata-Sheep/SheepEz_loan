933fd8c85811cd4cf021aa4814c4c776ea649127
road3144 <fnfnfn258@gmail.com>
2024-10-10 18:23:43 +0900

    fix: githubAction

--- user/user-api/src/test/java/com/sheep/ezloan/UserApplicationTest.java
+++ user/user-api/src/test/java/com/sheep/ezloan/UserApplicationTest.java
@@ -2,7 +2,7 @@ package com.sheep.ezloan;
 
 import org.junit.jupiter.api.Test;
 
-public class UserApplicationTest extends ContextTest {
+public class UserApplicationTest {
 
     @Test
     public void shouldBeLoadedContext() {
